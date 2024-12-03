6f567b63ab961ff6dcfc3deae65bc07e2ec2ec07
Jorge Pedraza <jorgepedraza1988@gmail.com>
2024-12-03 17:52:46 +0100

    new step

--- src/app/create-retro/components/create-retro-form.tsx
+++ src/app/create-retro/components/create-retro-form.tsx
@@ -1,5 +1,9 @@
 "use client";
-
+import {
+  HiPencil as EditIcon,
+  HiArrowRight as ArrowRightIcon,
+} from "react-icons/hi2";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
@@ -7,28 +11,67 @@ import { useState } from "react";
 
 export function CreateRetroForm() {
   const [step, setStep] = useState(1);
+  const [retroData, setRetroData] = useState({
+    adminId: "",
+    adminName: "",
+    date: new Date(),
+    timer: "",
+  });
 
-  const progressPercentage = (step / 4) * 100;
+  const progressPercentage = (step / 3) * 100;
 
   return (
     <div className="w-full">
       <div className="mt-4">
         <Progress value={progressPercentage} className="h-2" />
       </div>
-      <form className="mt-16">
+      <div className="mt-16">
         {step === 1 && (
-          <div className="flex justify-between items-center">
-            Select your avatar
+          <div>
+            <div className="w-full flex justify-center items-center">
+              <Avatar className="size-40 hover:cursor-pointer relative group">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>JP</AvatarFallback>
+                <div className="absolute size-full flex justify-center items-center invisible group-hover:visible group-hover:opacity-40 bg-black">
+                  <EditIcon size={32} className="text-white" />
+                </div>
+              </Avatar>
+            </div>
             <div className="w-full text-sm space-y-1">
               <label htmlFor="name">Name</label>
-              <Input id="name" placeholder="Enter your name" />
+              <Input
+                id="name"
+                placeholder="Enter your name"
+                value={retroData.adminName}
+                onChange={(e) =>
+                  setRetroData((prev) => ({
+                    ...prev,
+                    adminName: e.target.value,
+                  }))
+                }
+              />
             </div>
           </div>
         )}
-      </form>
-      <div className="mt-8 flex w-full justify-end gap-4">
+        {step === 2 && (
+          <div>
+            <h3 className="text-2xl">Choose your configuration</h3>
+            <ul className="list-disc">
+              <li>Limit the max number of participants</li>
+              <li>Set the timer: default 5 minutes</li>
+              <li>Allow votes</li>
+              <li>Enable chat</li>
+              <li>Set a password</li>
+            </ul>
+          </div>
+        )}
+      </div>
+      <div className="mt-4 flex w-full justify-end gap-4">
         {step > 1 && <Button onClick={() => setStep(step - 1)}>Back</Button>}
-        <Button onClick={() => setStep(2)}>Next</Button>
+        <Button onClick={() => setStep(step + 1)}>
+          Continue
+          <ArrowRightIcon size={24} />
+        </Button>
       </div>
     </div>
   );
--- src/app/create-retro/page.tsx
+++ src/app/create-retro/page.tsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
 import { CreateRetroForm } from "./components/create-retro-form";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)] h-screen flex justify-center">
       <div className="py-24">
-        <h1 className="text-4xl font-medium">Creating your Retrospective</h1>
+        <h1 className="text-4xl font-medium">Create your Retrospective</h1>
         <main className="flex flex-col justify-center items-center max-w-2xl">
           <CreateRetroForm />
+          <div className="text-xs text-gray-500 mt-8">
+            Don&apos;t want to create a retrospective?
+            <Button variant="link" className="text-sm" asChild>
+              <Link href="/">Go to main</Link>
+            </Button>
+          </div>
         </main>
       </div>
     </div>
